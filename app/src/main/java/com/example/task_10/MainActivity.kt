badf4ca7fba99197638f3fc36bc332da723d0c8b
alexderkach <derkachalex@i.ua>
2023-08-11 22:32:14 +0300

    reg activity in manifest

--- app/src/main/java/com/example/task_10/MainActivity.kt
+++ app/src/main/java/com/example/task_10/MainActivity.kt
@@ -2,19 +2,16 @@ package com.example.task_10
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity: Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
         val btn:Button = findViewById(R.id.button)
         btn.setOnClickListener(){
             Toast.makeText(this, "btn is pressed", Toast.LENGTH_SHORT).show()
         }
-
     }
-
 }
